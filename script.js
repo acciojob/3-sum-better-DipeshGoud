2a3cbc93dbe75748e176c64882dc654fa6b0885f
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-22 11:54:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function threeSum(arr, target) {
 // write your code here
+	let near = INFINITY;
+	for (let i = 0; i < arr.length-2; i++) {
+		for (let j = i+1; j < arr.length-1; j++) {
+			for (let k = j+1; k < arr.length; k++) {
+				if ((arr[i]+arr[j]+arr[k])-target < near) {
+					near = arr[i]+arr[j]+arr[k];
+				}
+			}
+		}		
+	}
+
+	return near
+	}
   
 }
 
