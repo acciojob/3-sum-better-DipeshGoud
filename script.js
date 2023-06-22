305c9a83215b5308c0108c3090682f00e2ec01f8
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-22 12:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function threeSum(arr, target) {
-// write your code here
-	let near = Infinity;
-	for (let i = 0; i < arr.length-2; i++) {
-		for (let j = i+1; j < arr.length-1; j++) {
-			for (let k = j+1; k < arr.length; k++) {
-				if (arr[i]+arr[j]+arr[k] >= 0 && (arr[i]+arr[j]+arr[k])-target < near) {
-					near = arr[i]+arr[j]+arr[k];
-				}
-			}
-		}		
-	}
-return near
+  let near = Infinity;
+  
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        const sum = arr[i] + arr[j] + arr[k];
+        if (Math.abs(sum - target) < Math.abs(near - target)) {
+          near = sum;
+        }
+      }
+    }
+  }
+  
+  return near;
 }
 
 module.exports = threeSum;
